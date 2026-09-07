53a39c47d091ed64929d04d08c411772fd48ee79
raheelUser123 <traheel05@gmail.com>
2026-09-07 16:25:43 +0500

    sss

--- includes/footer.php
+++ includes/footer.php
@@ -21,4 +21,18 @@
   <span class="whatsapp-tooltip">Chat with us</span>
 </a>
 
+  <!--Start of Tawk.to Script-->
+  <script type="text/javascript">
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/6a9e93dc27a69434428b87f6/1k1tn35hh';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+  </script>
+  <!--End of Tawk.to Script-->
+
 </body></html>
